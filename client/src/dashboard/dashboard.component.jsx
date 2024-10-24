3fc54166dd244bf16445579e3271477e66b1547c
Carolina <boitelc@ufl.edu>
2024-10-24 11:56:52 -0400

    mockup add habits dialog

--- client/src/dashboard/dashboard.component.jsx
+++ client/src/dashboard/dashboard.component.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './dashboard.component.css'; // Make sure to import your CSS
+import './dashboard.component.css';
 
 function Dashboard() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -27,9 +27,13 @@ function Dashboard() {
   return (
     <div>
       <div className="header">
-        {getGreeting()}, Alexa
+        {getGreeting()}, Alexa       
       </div>
       <p className='text'>Today is {getCurrentDate()}</p>
+      <div>
+        <img src="https://cdn-icons-png.flaticon.com/512/561/561135.png" alt="Settings Icon" className="settings-icon" />
+        </div>
+        <div><img src="https://static-00.iconduck.com/assets.00/profile-icon-512x512-w0uaq4yr.png" alt="Profile Icon" className="profile-icon"/></div>
       <div className="dashboard-container">
         {/* Boxes with tasks */}
         <div className="box" style={{ backgroundColor: '#DBCDF0' }}>
@@ -81,11 +85,22 @@ function Dashboard() {
       {/* Dialog */}
       {isDialogOpen && (
         <div className="dialog">
-          <div className="dialog-content">
-            <h2>Add New Habit</h2>
-            {/* Add form or content here */}
-            <button onClick={toggleDialog}>Close</button>
-          </div>
+            <div className="dialog-content">
+                <h2>Choose New Habit</h2>
+                <button className="add-habit" style={{ backgroundColor: '#DBCDF0' }}>
+                    <div className="habit-title">Finance</div>
+                </button>
+                <button className="add-habit" style={{ backgroundColor: '#F7D9C4' }}>
+                    <div className="habit-title">Academics</div>
+                </button>
+                <button className="add-habit" style={{ backgroundColor: '#FAEDCB' }}>
+                    <div className="habit-title">Gym</div>
+                </button>
+                <button className="add-habit" style={{ backgroundColor: '#F2C6DE' }}>
+                    <div className="habit-title">Home</div>
+                </button>
+                <button className="done-button" onClick={toggleDialog}>Done</button>
+            </div>
         </div>
       )}
     </div>
