452f61c8d3051cc1a3de5b6953c3887d059c3364
carloschavez <cforcarlos22@gmail.com>
2024-10-28 11:55:17 -0400

    Made it so you can add tasks to dashboard and succefully add tasks to database.

--- server/routes/record.js
+++ server/routes/record.js
@@ -5,10 +5,22 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// Initial route for testing
-router.get("/", (req, res) => {
-  res.send("GET all user information!");
+router.get("/", async (req, res) => {
+  try {
+    const db = await connectDB();
+    const collection = db.collection("records");
+    const tasks = await collection.find({}).toArray();
+    res.status(200).json(tasks);
+  } catch (err) {
+    console.error("Error retrieving tasks:", err);
+    res.status(500).send("Error retrieving tasks");
+  }
 });
+
+// Initial route for testing
+// router.get("/", (req, res) => {
+//   res.send("GET all user information!");
+// });
 
 // This section will help you get a list of all the user info
 router.get("/all", async (req, res) => { // Adjusted path to avoid duplication
@@ -106,14 +118,19 @@ router.post("/add-task", async (req, res) => {
       habit: req.body.habit,
       task: req.body.task,
     };
-    const collection = db.collection("userinfo");
+    console.log("New Habit Document:", newHabit);
+    const collection = db.collection("records");
     const result = await collection.insertOne(newHabit);
-    res.status(201).send(result);
+    const insertedTask = await collection.findOne({ _id: result.insertedId });
+
+    res.status(201).send(insertedTask);
   } catch (err) {
     console.error("Error adding task:", err);
     res.status(500).send("Error adding task");
   }
 });
+
+
 
 export default router;
 
