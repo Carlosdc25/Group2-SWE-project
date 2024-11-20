39caa8fbfb65cdb9c1354d3a8aad2b0d4d456182
Ashley M <ashleyemaurer@gmail.com>
2024-11-20 12:05:34 -0500

    reset backend files

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,34 +1,26 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-import dotenv from "dotenv";
-dotenv.config();
 
 const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-let db;
-
-async function connectDB() {
-  if (db) return db; // Return existing connection
-
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-
-  try {
-    await client.connect();
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-
-    db = client.db("users");
-    return db;
-  } catch (err) {
-    console.error(err);
-    throw err; // Let the caller handle the error
-  }
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+   "Pinged your deployment. You successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
 }
 
-export default connectDB;
+let db = client.db("employees");
+
+export default db;
